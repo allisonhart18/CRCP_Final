3302c8808d6365bcf751edcb3b71ff2b282b0175
allisonhart18 <adhart@smu.edu>
2025-03-30 16:40:17 -0500

    third commit

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,7 +1,24 @@
+let noiseScale = .02;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
+  noLoop(); // only draw once for rn
 }
 
 function draw() {
-  background(220);
+  background(150);
+
+  loadPixels();
+  for (let y = 0; y < height; y++){
+    for (let x = 0; x < width; x++){
+      let noiseValue = noise(x * noiseScale, y * noiseScale);
+      let colorValue =  map(noiseValue, 0, 1, 0, 255);
+      let index = (x + y *width) *4;
+      pixels[index] = colorValue; // red?
+      pixels [ index + 1] = colorValue; // green?
+      pixels [ index + 2] = colorValue; // blue?
+      pixels[ index + 3] = 255; //Alpha???
+    }
+  }
+  updatePixels();
 }
