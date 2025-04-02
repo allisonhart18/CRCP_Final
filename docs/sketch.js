d52f3818505807a797638a8eaacb53fbd553fdbb
allisonhart18 <adhart@smu.edu>
2025-04-01 19:22:39 -0500

    trying again to add lines, not noise

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,45 +1,43 @@
-let noiseScale = .01;
-//most info from watching daniel shiffman videos on youtube :)
-
 function setup() {
-  createCanvas(windowWidth*.75, windowHeight*.75);
+  createCanvas(windowWidth*0.75,windowHeight*0.75);
   background(150);
-  strokeWeight(.4); //maybe bigger??
+  strokeWeight(0.6);
+  //noiseSeed(1000);
   frame = 0;
-  numVar = 40;
-  rez3 = .002;
-  len = size1 *.3;
+  numAcross = 40;
+  size1 = (width - frame * 2) / numAcross;
+  rez3 = 0.003;
+  len = size1 * 0.3;
   stroke(0, 200);
-  size1(width - frame *2) / numVar;
   lines = [];
   lines2 = [];
-    for(x = frame; x < width - frame; x += size1){
-      for( y = frame; y < height - frame; y += size1){
-        x+= width * random(-.001, .001);
-        y += width * random(-.001, .001);
-        pastX = x;
-        oldY = y;
-        for (i = 0; i < 20; i++) {
-
-          n = noise(pastX * rez3, oldY *rez3) + .033;
-          ang = map(n, 0.3, .7, 0, PI *2);
-
-          newX = cos(ang) * len + pastX;
-          newY = sin(ang) * len + pastY;
-          line(pastX, pastY, newX, newY);
-          pastX = newX;
-          pastY = newY;
-
-        }
-
-      }
+  for (x = frame; x < width - frame + 1; x += size1) {
+    for (y = frame; y < height - frame + 1; y += size1) {
+      lines.push(x, y);
     }
- //z = 800;
-
-function getColor(col1){
-
-  h = int(table.get(palette, col1 * 3));
-  s = int(table.get(palette, col1 * 3 +1));
-  b = int(table.get(palette, col1 * + 2));
+  }
+  z = 1000;
 }
+
+function draw() {
+  background(150); // add alpha???
+  for (j = 0; j < lines.length; j += 2) {
+    oldX = lines[j];
+    oldY = lines[j + 1];
+    n3 = noise(oldX * rez3, oldY * rez3,z*rez3) + 0.033;
+    ang = map(n3, 0.3, 0.7, 0, PI * 2);
+    //ang = n3*PI*2; // no mapping - flows left
+    newX = cos(ang) * len + oldX;
+    newY = sin(ang) * len + oldY;
+    line(oldX, oldY, newX, newY);
+    if ((newX > width || newX < 0) && (newY > height || newY < 0)) {
+      newX = random(width);
+      newY = random(height);
+    }
+    lines2.push(newX, newY);
+  }
+  lines = lines2;
+  lines2 = [];
+  z+=2;
+  for (q = 0; q < 10000; q++) {} // can substitute 1e5 for 10000
 }
