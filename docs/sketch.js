1350720b9662683cc60e950341f0f5dea952cfaf
allisonhart18 <adhart@smu.edu>
2025-04-08 18:27:20 -0500

    Fixing line weight and movement

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,14 +1,13 @@
 function setup() {
   createCanvas(windowWidth*0.75,windowHeight*0.75);
   background(150);
-  strokeWeight(0.6);
-  //noiseSeed(1000);
+  strokeWeight(.7);
   frame = 0;
   numAcross = 40;
   size1 = (width - frame * 2) / numAcross;
   rez3 = 0.003;
-  len = size1 * 0.3;
-  stroke(0, 200);
+  len = size1 * 0.4;
+  stroke(0, 250);
   lines = [];
   lines2 = [];
   for (x = frame; x < width - frame + 1; x += size1) {
@@ -20,13 +19,13 @@ function setup() {
 }
 
 function draw() {
-  background(150); // add alpha???
+  background(150,15); // add alpha for transparent background??
   for (j = 0; j < lines.length; j += 2) {
     oldX = lines[j];
     oldY = lines[j + 1];
     n3 = noise(oldX * rez3, oldY * rez3,z*rez3) + 0.033;
     ang = map(n3, 0.3, 0.7, 0, PI * 2);
-    //ang = n3*PI*2; // no mapping - flows left
+    
     newX = cos(ang) * len + oldX;
     newY = sin(ang) * len + oldY;
     line(oldX, oldY, newX, newY);
@@ -39,5 +38,5 @@ function draw() {
   lines = lines2;
   lines2 = [];
   z+=2;
-  for (q = 0; q < 10000; q++) {} // can substitute 1e5 for 10000
+ // for (q = 0; q < 10000; q++) {} 
 }
